277c8d224ae188f217962327755781c111ca6139
Andrei <andrei.lerke@gmail.com>
2022-06-24 22:19:55 +0200

    gestaltet

--- js/task-03.js
+++ js/task-03.js
@@ -1,9 +1,10 @@
 const ADMIN_PASSWORD = 'jqueryismyjam';
-let message = prompt('Введите пароль:');
+let message;
+const password = prompt('Введите пароль:');
 
-if (message === null) {
+if (password === null) {
   message = 'Отменено пользователем!';
-} else if (message === ADMIN_PASSWORD) {
+} else if (password === ADMIN_PASSWORD) {
   message = 'Добро пожаловать!';
 } else {
   message = 'Доступ запрещен, неверный пароль!';
